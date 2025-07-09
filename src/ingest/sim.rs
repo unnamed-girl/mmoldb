469016f14564967991b178680d42563ac11b1139
unnamedgirl <unnamedgirl.public@gmail.com>
2025-07-09 10:52:15 +0000

    Adjust NotRecognized handling again

--- src/ingest/sim.rs
+++ src/ingest/sim.rs
@@ -52,8 +52,8 @@ pub enum SimStartupError {
         received: ParsedEventMessageDiscriminants,
     },
 
-    #[error("Couldn't parse game day: {}", day.0)]
-    FailedToParseGameDay { day: mmolb_parsing::NotRecognized },
+    #[error("Couldn't parse game day")]
+    FailedToParseGameDay { source: mmolb_parsing::NotRecognized },
 
     #[error("Couldn't parse starting pitcher \"{0}\"")]
     FailedToParseStartingPitcher(String),
@@ -1095,9 +1095,9 @@ impl<'g> Game<'g> {
             season: game_data.season.into(),
             day: match &game_data.day {
                 Ok(day) => *day,
-                Err(day) => {
+                Err(error) => {
                     return Err(SimStartupError::FailedToParseGameDay {
-                        day: day.clone(),
+                        source: error.clone(),
                     });
                 }
             },
